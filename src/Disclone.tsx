4c28dd64b3c7a1f8976271f702afafb57bc9822d
Angel Rios <riosmartinezkg@gmail.com>
2026-05-26 19:42:16 -0600

    Feat: QueryCLient added to Disclone.tsx

--- src/Disclone.tsx
+++ src/Disclone.tsx
@@ -1,12 +1,18 @@
 import { RouterProvider } from "react-router"
 
 import { appRouter } from './router/app.router';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+
+const queryClient = new QueryClient();
 
 export const Disclone = () => {
   return (
     <>
-      <RouterProvider router={appRouter} />
+      <QueryClientProvider client={queryClient}>
+
+        <RouterProvider router={appRouter} />
+      </QueryClientProvider>
 
 
     </>
--- src/auth/pages/AuthPage.tsx
+++ src/auth/pages/AuthPage.tsx
@@ -7,21 +7,21 @@ import { Login } from "../components/login/Login";
 
 
 export const AuthPage = () => {
-    const [isLogin, setIsLogin] = useState(true); 
+  const [isLogin, setIsLogin] = useState(true);
   return (
-    
+
 
     <>
 
-    {isLogin?<Login isLogin={isLogin} setIsLogin={setIsLogin}/>:<Signup isLogin={isLogin} setIsLogin={setIsLogin}/>}
+      {isLogin ? <Login isLogin={isLogin} setIsLogin={setIsLogin} /> : <Signup isLogin={isLogin} setIsLogin={setIsLogin} />}
 
-    <Button  onClick={()=> setIsLogin(!isLogin)}>
+      <Button onClick={() => setIsLogin(!isLogin)}>
 
-         {isLogin ? '¿No tienes cuenta? Regístrate' : '¿Ya tienes cuenta? Inicia sesión'}
+        {isLogin ? '¿No tienes cuenta? Regístrate' : '¿Ya tienes cuenta? Inicia sesión'}
 
-    </Button>
-    
-    
+      </Button>
+
+
     </>
   )
 }
