3e9224079b9b973c3c637a499ab900c8c86218cc
Angel Rios <riosmartinezkg@gmail.com>
2026-05-26 20:56:16 -0600

    Feat: Singup.tsx and Logim.tsx forms now send auth data correctly

---
+++ src/app/actions/post-signup.ts
@@ -0,0 +1,26 @@
+import { backendPath } from "../../api/backend-path.api";
+
+
+interface SignupData {
+  name: string;
+  email: string;
+  password: string;
+  repeatPassword: string;
+}
+
+
+export const  postSignup = async({name, email, password, repeatPassword}: SignupData) =>{
+
+    const {data} = await backendPath.post('/auth/signup',{
+        name,
+        email,
+        password,
+        repeatPassword
+    }); 
+
+
+    return {
+        data
+  
+    };
+}
--- src/app/auth/components/signup/Signup.tsx
+++ src/app/auth/components/signup/Signup.tsx
@@ -1,11 +1,21 @@
+import { postSignup } from "@/app/actions/post-signup"
 import { Button } from "@/components/ui/button"
 import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useMutation } from "@tanstack/react-query"
+
 
 interface Props {
   isLogin: boolean,
   setIsLogin: React.Dispatch<React.SetStateAction<boolean>>,
+}
+
+interface SignupData {
+  name: string;
+  email: string;
+  password: string;
+  repeatPassword: string;
 }
 
 export const Signup = ({ isLogin, setIsLogin }: Props) => {
@@ -13,21 +23,62 @@ export const Signup = ({ isLogin, setIsLogin }: Props) => {
   const handlePage = () => {
     setIsLogin(!isLogin)
   }
+  const mutation = useMutation({
+    mutationFn: ({
+      name,
+      email,
+      password,
+      repeatPassword,
+    }: SignupData) => postSignup({name, email, password, repeatPassword}),
+  });
+
+  const handleSubmit =  (
+    e: React.FormEvent<HTMLFormElement>
+  ) => {
+    e.preventDefault();
+
+
+    const formData = new FormData(e.currentTarget);
+    const name = formData.get("name") as string;
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const repeatPassword = formData.get("repeat-password") as string;
+
+
+    mutation.mutate(
+      {
+        name,
+        email,
+        password,
+        repeatPassword,
+      },
+      {
+        onSuccess: (data) => {
+          console.log("SUCCESS", data);
+        },
+
+        onError: (error) => {
+          console.log("ERROR", error);
+        },
+      }
+    );
+
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen">
 
       <Card className="w-full max-w-sm">
-        <form>
-        <CardHeader>
-          <CardTitle>Create an account</CardTitle>
-          <CardDescription>
-            Enter your details below to create your account
-          </CardDescription>
-        </CardHeader>
+        <form onSubmit={handleSubmit}>
+          <CardHeader>
+            <CardTitle>Create an account</CardTitle>
+            <CardDescription>
+              Enter your details below to create your account
+            </CardDescription>
+          </CardHeader>
 
-        <CardContent>
-          
+          <CardContent>
+
             <div className="flex flex-col gap-6">
 
               {/* Name */}
@@ -35,6 +86,7 @@ export const Signup = ({ isLogin, setIsLogin }: Props) => {
                 <Label htmlFor="name">Name</Label>
                 <Input
                   id="name"
+                  name="name"
                   type="text"
                   placeholder="John Doe"
                   required
@@ -46,6 +98,7 @@ export const Signup = ({ isLogin, setIsLogin }: Props) => {
                 <Label htmlFor="email">Email</Label>
                 <Input
                   id="email"
+                  name="email"
                   type="email"
                   placeholder="m@example.com"
                   required
@@ -55,29 +108,33 @@ export const Signup = ({ isLogin, setIsLogin }: Props) => {
               {/* Password */}
               <div className="grid gap-2">
                 <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" required />
+                <Input id="password" name="password" type="password" required />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Repeat Password</Label>
+                <Input id="repeat-password" name="repeat-password" type="repeat-password" required />
               </div>
 
             </div>
-         
-        </CardContent>
 
-        <CardFooter className="flex-col gap-2">
-          <Button type="submit" className="w-full">
-            Sign up
-          </Button>
+          </CardContent>
 
-          {/* <Button variant="outline" className="w-full">
+          <CardFooter className="flex-col gap-2">
+            <Button type="submit" className="w-full">
+              Sign up
+            </Button>
+
+            {/* <Button variant="outline" className="w-full">
             Sign up with Google
           </Button> */}
 
-          <CardAction>
-            <Button variant="link" onClick={handlePage}>
-              Already have an account? Login
-            </Button>
-          </CardAction>
-        </CardFooter>
-         </form>
+            <CardAction>
+              <Button type="button" variant="link" onClick={handlePage}>
+                Already have an account? Login
+              </Button>
+            </CardAction>
+          </CardFooter>
+        </form>
       </Card>
 
     </div>
