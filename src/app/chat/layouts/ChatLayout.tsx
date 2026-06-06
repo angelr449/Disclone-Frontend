8da2673622e037a08609ad62da5635411d23c8ce
Angel Rios <riosmartinezkg@gmail.com>
2026-06-05 21:22:03 -0600

    Feat and Refactor: CustomSidebarChat.tsx modifed and src/app/layout/AppLayout.tsx created

--- src/app/chat/layouts/ChatLayout.tsx
+++ src/app/chat/layouts/ChatLayout.tsx
@@ -1,22 +1,14 @@
-import {
-  SidebarProvider,
-  SidebarInset,
-} from "@/components/ui/sidebar"
+
 // 👈 agregar
 import { CustomChatHeader } from "../components/CustomChatHeader"
 import { ChatPage } from "../pages/ChatPage"
-import { CustomSidebarChat } from '../../components/CustomSidebarChat';
+
 
 export const ChatLayout = () => (
   <div className="h-screen bg-[#36393f] text-white"> 
-    <SidebarProvider>
-      <CustomSidebarChat /> {/* 👈 esto faltaba */}
-      <SidebarInset className="flex flex-col flex-1 min-w-0">
-        <CustomChatHeader />
+<CustomChatHeader />
         <main className="flex-1 overflow-hidden h-screen bg-[#36393f] text-white">
           <ChatPage />
         </main>
-      </SidebarInset>
-    </SidebarProvider>
   </div>
 )
